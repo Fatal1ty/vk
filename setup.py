6c71bbc5ca743ddb7d4c6edcc2093d66d6b3cc37
Alexander Tikhonov <random.gauss@gmail.com>
2013-03-21 18:03:40 +0400

    Added setup script and reorganized structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(name='vk',
+      version='1.0',
+      description='Package for interaction with vk.com API',
+      author='Alexander Tikhonov',
+      author_email='random.gauss@gmail.com',
+      url='https://github.com/Fatal1ty/vk',
+      packages=['vk'],
+      package_dir={'': 'src'})
